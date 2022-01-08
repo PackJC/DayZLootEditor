68bba19127d4be2534490a667038c101d5724629
Johnny Pack <34726562+PackJC@users.noreply.github.com>
2022-01-07 23:58:33 -0500

    Added events

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow, remote} = require('electron')
+
+const electron = require("electron");
+const { app, BrowserWindow ,ipcMain,dialog} = electron;
 const path = require('path')
 const fs = require('fs');
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     autoHideMenuBar: true,
     icon: './media/DayZLogo.PNG',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      enableRemoteModule: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      nodeIntegrationInWorker: true,
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools();
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -44,9 +51,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-// Importing dialog module using remote
+// Use local web browser to open links
 app.on('web-contents-created', (e, contents) => {
   contents.on('new-window', (e, url) => {
     e.preventDefault();
@@ -56,3 +61,62 @@ app.on('web-contents-created', (e, contents) => {
     if (url !== contents.getURL()) e.preventDefault(), require('open')(url);
   });
 });
+
+ipcMain.on("getFile",(event)=>{
+  options={
+    title:"Select XML Files to import",
+    buttonLabel:"Import .XML File",
+    defaultPath:app.getPath('desktop'),
+    properties:['openFile','multiSelections'],
+
+    // See place holder 4 in above image
+    filters :[
+      {name: 'Text', extensions: ['txt', 'text', 'png']},
+      {name: 'XML', extensions: ['xml']}
+        ],
+
+  }
+  dialog.showOpenDialog(options).then((result)=>{
+    console.warn("res",result)
+    console.log("Result" + result.toString())
+    pages=result.filePaths
+    console.warn("Line 76: " + pages)
+    if(false === result.canceled){
+      event.reply("reply",result) //This is how we send the file back
+    }
+    else{
+      console.warn("file not selected")
+    }
+    console.log('Line 84'  )
+  })
+})
+
+ipcMain.on("whereto",(event)=>{
+  options={
+    title:"select the file",
+    defaultPath:"Untitled",
+  }
+  dialog.showSaveDialog(options).then((result)=>{
+    out=result.filePath+".pdf"
+    console.warn(out)
+    if(false === result.canceled){
+      event.reply("imgtopdf","true")
+      console.warn("test2")
+    }
+    else{
+      console.warn("file not selected")
+    }
+    console.warn("test3")
+  })
+})
+
+ipcMain.on("imgtopdf_convert",(event)=>{
+  console.warn("started converting imgtopdf")
+  console.warn(pages)
+  console.warn(out)
+  imgToPDF(pages, 'A4').pipe(fs.createWriteStream(out));
+  event.reply("imgtopdfconvertfinal","true")
+  console.warn("test4")
+})
+
+
--- renderer.js
+++ renderer.js
@@ -1,30 +1,6 @@
 
+// Change theme onclick
 
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-
-var input = document.querySelector('input');
-input.type = 'file';
-input.onchange = e => {
-
-    // getting a hold of the file reference
-    var file = e.target.files[0];
-
-    // setting up the reader
-    var reader = new FileReader();
-    reader.readAsText(file,'UTF-8');
-
-    // here we tell the reader what to do when it's done reading...
-    reader.onload = readerEvent => {
-        var content = readerEvent.target.result; // this is the content!
-        console.log( content );
-        var element = document.getElementById("myPara");
-        element.innerHTML = content;
-    }
-}
-input.click();
-
+// document.getElementById("new").onclick = () => {
+//   document.getElementById("new").style.color = "blue";
+// };
